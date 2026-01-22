ef06867a76eacd129f6f75d9bf1293a3bbbb20ea
zoomdong <1344492820@qq.com>
2026-01-22 16:43:44 +0800

    chore: bump umi & utoopack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import yayJpg from '../assets/yay.jpg';
 import testMp4 from '../assets/oceans.mp4';
+import styles from './index.less';
+import styles2 from '@/layouts/index.less';
 
 export async function getTMPJson() {
   const x = import('../assets/Regular Expressions (JavaScript).tmLanguage.json');
@@ -10,8 +12,8 @@ export async function getTMPJson() {
 export default function HomePage() {
   return (
     <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
+      <h2 className={styles.displayName}>Yay! Welcome to umi!</h2>
+      <p className={styles2.navs}>
         <video 
           width="640" 
           height="360"  
